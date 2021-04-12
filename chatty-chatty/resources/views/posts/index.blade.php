d9d7be06ab0280c4a2511b64b9316df314489a04
nnsncl <nuniroland@gmail.com>
2021-04-12 14:19:50 +0200

    remove archived code

--- chatty-chatty/resources/views/posts/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts/app')
-
-@section('content')
-    <div class="flex justify-center" >
-        <div class="w-10/12 bg-gray-100 p-6 rounded-lg" >
-            Post
-        </div>
-    </div>
-@endsection
